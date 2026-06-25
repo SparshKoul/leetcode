25e1803515ddbf2a4528492972e9f299ff9d1f7c
Sparsh Koul <sparshkoul6@gmail.com>
2026-06-25 20:16:39 +0530

    Time: 1 ms (99.94%), Space: 94.6 MB (15.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,28 +1,17 @@
-// class Solution {
-//     public int maxProfit(int[] prices) {
-        
-//     }
-// }
-public class Solution {
-    public int maxProfit(int prices[]) {
-        int maxprofit = 0;
-        int buyprice = Integer.MAX_VALUE;
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit=0;
+        int bp=Integer.MAX_VALUE;
 
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] > buyprice) {//sp>bp
-                int profit = prices[i] - buyprice;
-                maxprofit = Math.max(maxprofit, profit);//compare honge profit or jo bda uske value sace
-            } 
-            else   {
-                buyprice = prices[i];
-                //as buy price is>sp  so we can buy and keep it for future
-                
+        for(int i=0;i<prices.length;i++){
+            if(prices[i] > bp){
+                int profit=prices[i]- bp;
+                maxprofit=Math.max(profit,maxprofit);
+            }
+            else{
+                bp = prices[i];
             }
         }
-
         return maxprofit;
     }
-    
 }
-
-
