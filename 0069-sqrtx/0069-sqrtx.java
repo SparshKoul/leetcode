f68a7958c0c4d5222cebb91a2c25baddfe9f2697
Sparsh Koul <sparshkoul6@gmail.com>
2026-06-25 20:59:27 +0530

    Time: 1 ms (97.81%), Space: 42.7 MB (41.93%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        // ini guessr=x
+        long r = x;
+        
+        while (r * r > x) {
+            // Newton's update formula: r = (r + x / r) / 2
+            r = (r + x / r) / 2;
+        }
+
+        return (int) r;
+    }
+}
