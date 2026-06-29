811eb905f85d129739251ab88bb8365cab4e4c37
Sparsh Koul <sparshkoul6@gmail.com>
2026-06-29 20:43:57 +0530

    Time: 13 ms (36.34%), Space: 45.1 MB (42.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,22 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         for (int i = 0; i < s.length() / 2; i++) {
+//             if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
-
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return false;
             }
-            left++;
-            right--;
         }
-
         return true;
     }
 }
