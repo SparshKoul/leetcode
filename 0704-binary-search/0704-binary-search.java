89655ec79daf5c77096e46e8f7d588112b05f12f
Sparsh Koul <sparshkoul6@gmail.com>
2026-06-30 20:52:04 +0530

    Time: 0 ms (100%), Space: 48.5 MB (30.84%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,10 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] ==target){
-                return i;
+        int low = 0;
+        int high = nums.length - 1;
+        while(low<=high){
+            int mid=low + (high-low )/2;
+            if(nums[mid]== target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
-        return -1;
+    return  -1;
+        
     }
 }
