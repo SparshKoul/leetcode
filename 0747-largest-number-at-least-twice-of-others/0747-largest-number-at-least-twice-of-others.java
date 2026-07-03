1667e0863981e6717bbdb2e46be2499e8491a3da
Sparsh Koul <sparshkoul6@gmail.com>
2026-07-03 20:29:49 +0530

    Time: 3 ms (8.63%), Space: 43.5 MB (8.88%) - LeetHub

---
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int dominantIndex(int[] arr) {
+        
+        int n = arr.length;
+        int largest = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = 0; i < n; i++){
+            if (arr[i] > largest) {
+                largest = arr[i];
+                index = i;
+            }
+        }
+        int secondlargest = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > secondlargest && largest != arr[i]) {
+                secondlargest = arr[i];
+            }
+        }
+        System.out.println(secondlargest);
+
+        if (largest >= (secondlargest * 2)) {
+            return index;
+        }
+        return -1;
+
+    }
+}
