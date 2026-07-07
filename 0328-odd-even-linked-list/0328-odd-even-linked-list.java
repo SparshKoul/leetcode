af8903f99748bc357e9f8508fd8875e7891457ed
Sparsh Koul <sparshkoul6@gmail.com>
2026-07-08 01:03:27 +0530

    Time: 0 ms (100%), Space: 46.6 MB (16.52%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,45 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
- class Solution {
+class Solution {
     public ListNode oddEvenList(ListNode head) {
 
-        if (head == null) {
-            return null;
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+
+            odd.next = odd.next.next;
+            odd = odd.next;
+
+            even.next = even.next.next;
+            even = even.next;
         }
 
-        ArrayList<ListNode> list = new ArrayList<>();
+        odd.next = evenHead;
 
-        ListNode temp = head;
-
-        while (temp != null) {
-            list.add(temp);
-            temp = temp.next;
-        }
-
-        ListNode curr = list.get(0);
-
-        // Odd posit 1st, 3, 5...
-        for (int i = 2; i < list.size(); i += 2) {
-            curr.next = list.get(i);
-            curr = curr.next;
-        }
-
-        // Even pos: 2, 4, 6...
-        for (int i = 1; i < list.size(); i += 2) {
-            curr.next = list.get(i);
-            curr = curr.next;
-        }
-
-        curr.next = null;
-
-        return list.get(0);
+        return head;
     }
 }
