e79bb685db6d9c5f3108a0e97c2c9b785094f9dc
Sparsh Koul <sparshkoul6@gmail.com>
2026-07-21 17:06:31 +0530

    Time: 1 ms (97.52%), Space: 42.8 MB (23.6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,17 +1,26 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0) {
-            return 0;
+        if (x == 0 || x == 1) {
+            return x;
         }
+        int low = 1;
+        int high = x;
+        int ans = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long sq = (long) mid * mid;
 
-        // ini guessr=x
-        long r = x;
-        
-        while (r * r > x) {
-            // Newton formula: r = (r + x / r) / 2
-            r = (r + x / r) / 2;
+            if (sq == x) {
+                return mid;
+            }
+            if (sq < x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
+        return ans;
 
-        return (int) r;
     }
 }
