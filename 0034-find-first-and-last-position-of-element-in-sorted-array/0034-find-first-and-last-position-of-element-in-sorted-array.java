968f20da4c2536e7f8e71a3033947f0c6b159b20
Sparsh Koul <sparshkoul6@gmail.com>
2026-08-04 17:35:38 +0530

    Time: 0 ms (100%), Space: 48.1 MB (72.28%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,50 +1,48 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int firsttt = first(nums, target);
-        int lasttt = last(nums, target);
-
+    public int[] searchRange(int[] arr, int target) {
+        int firsttt = first(arr, target);
+        int lasttt = last(arr, target);
         return new int[] { firsttt, lasttt };
 
     }
 
-    public int first(int nums[], int target) {
+    public int first(int[] arr, int target) {
+        int n = arr.length;
         int low = 0;
-        int high = nums.length - 1;
+        int high = n - 1;
         int ans = -1;
-
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
+            if (arr[mid] == target) {
                 ans = mid;
                 high = mid - 1;
-            } else if (nums[mid] < target) {
+
+            } else if (arr[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
-
         }
         return ans;
     }
 
-    public int last(int nums[], int target) {
+    public int last(int[] arr, int target) {
+        int n = arr.length;
         int low = 0;
-        int high = nums.length - 1;
+        int high = n - 1;
         int ans = -1;
-
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
+            if (arr[mid] == target) {
                 ans = mid;
                 low = mid + 1;
-            } else if (nums[mid] < target) {
+
+            } else if (arr[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
-
         }
         return ans;
-
     }
 }
