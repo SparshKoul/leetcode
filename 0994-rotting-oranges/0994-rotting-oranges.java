dc6671d944e5614e296b1dd1785b457fedd7858a
Sparsh Koul <sparshkoul6@gmail.com>
2026-08-18 20:31:19 +0530

    Time: 2 ms (89.55%), Space: 43.6 MB (97.93%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int fresh = 0;
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[] { i, j });
+                }
+                if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int time = 0;
+
+        while (!q.isEmpty() && fresh > 0) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int curr[] = q.poll();
+
+                int r = curr[0];
+                int c = curr[1];
+
+                //up
+                if (r - 1 >= 0 && grid[r - 1][c] == 1) {
+                    grid[r - 1][c] = 2;
+                    fresh--;
+                    q.add(new int[] { r - 1, c });
+                }
+
+                //down
+                if (r + 1 < rows && grid[r + 1][c] == 1) {
+                    grid[r + 1][c] = 2;
+                    fresh--;
+                    q.add(new int[] { r + 1, c });
+                }
+
+                //leftt
+                if (c - 1 >= 0 && grid[r][c - 1] == 1) {
+                    grid[r][c - 1] = 2;
+                    fresh--;
+                    q.add(new int[] { r, c - 1 });
+                }
+
+                //right
+                if (c + 1 < cols && grid[r][c + 1] == 1) {
+                    grid[r][c + 1] = 2;
+                    fresh--;
+                    q.add(new int[] { r, c + 1 });
+                }
+
+            }
+            time++;
+        }
+        if (fresh == 0) {
+            return time;
+        }
+        return -1;
+    }
+}
