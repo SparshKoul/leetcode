23fad98e0a4fbee3e1c6b8da35ccb60a838bbd83
Sparsh Koul <sparshkoul6@gmail.com>
2026-08-19 20:12:22 +0530

    Time: 13 ms (32.07%), Space: 52.3 MB (97.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -7,6 +7,10 @@ class Solution {
 
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
+            
+        }
+
+        for(int num:map.keySet()){
             if (map.get(num) > limit) {
                 return num;
             }
