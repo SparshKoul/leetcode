6650b76b50db7401baa1b45c2175cec4ad6c1090
Sparsh Koul <sparshkoul6@gmail.com>
2026-08-19 21:06:29 +0530

    Time: 35 ms (43.05%), Space: 60.3 MB (9.62%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,26 @@
-import java.util.*;
-
-public class Solution {
-    public static List<List<Integer>> threeSum(int[] nums) {
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++) {
-
-            // skp dupli i
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            int left = i + 1;
+            int left = i + 1; // +1 as nums[i] is selected as first
             int right = nums.length - 1;
 
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
 
-                    // skp dupli
+                    res.add(list);
+
                     while (left < right && nums[left] == nums[left + 1])
                         left++;
                     while (left < right && nums[right] == nums[right - 1])
@@ -28,13 +28,21 @@ public class Solution {
 
                     left++;
                     right--;
-                } else if (sum < 0) {
+
+                }
+
+                else if (sum < 0) {
+
                     left++;
+
                 } else {
+
                     right--;
                 }
             }
         }
         return res;
+
     }
+
 }
