fca9b5405f37c0222937251571f86b939f91d710
Sparsh Koul <sparshkoul6@gmail.com>
2026-08-27 04:47:53 +0530

    Time: 1 ms (86.85%), Space: 47.2 MB (74.41%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,32 +1,31 @@
 class Solution {
-    public void bfs(boolean[] visited, int[][] graph, int i){
-        visited[i] = true;
-        int n=graph.length;
+    public void bfs(boolean[] visited,int[][] isConnected, int startcity){
         Queue<Integer> q = new LinkedList<>();
-        q.add(i);
+        int n =isConnected.length;
+        q.add(startcity);
+        visited[startcity] =true;
 
         while(q.size() >0){
             int front =q.remove();
             for(int j=0;j<n;j++){
-                if(graph[front][j] ==1 && visited[j] ==false){
+                if(isConnected[front][j] ==1 && visited[j] ==false){
                     q.add(j);
                     visited[j]=true;
                 }
             }
         }
     }
-    public int findCircleNum(int[][] arr) {
-        int n=arr.length; //row here n*n so same thing
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length; //row here n*n so same thing
         int count =0;
         boolean [] vis =new boolean [n];
         for(int i=0;i<n;i++){
             if( !vis[i]){
-                bfs(vis,arr,i);
+                bfs(vis,isConnected,i);
                 count++;
             }
         }
         return count;
-
         
     }
 }
