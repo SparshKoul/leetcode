af72f890a11c8d23376f3634d096db653491be24
Sparsh Koul <sparshkoul6@gmail.com>
2026-08-27 20:34:46 +0530

    Time: 0 ms (100%), Space: 119.5 MB (7.07%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,57 +1,44 @@
 class Solution {
 
-    // index of max el in row 
-    public int maxElement(int[][] arr, int row) {
-        int m = arr[0].length;
+    public int max(int[][] mat, int row) {
         int max = Integer.MIN_VALUE;
         int index = -1;
+        int m = mat[0].length;
 
-        for (int j = 0; j < m; j++) {
-            if (arr[row][j] > max) {
-                max = arr[row][j];
-                index = j;
+        for (int i = 0; i < m; i++) {
+            if (mat[row][i] > max) {
+                max = mat[row][i];
+                index = i;
             }
         }
         return index;
     }
 
-    public int[] findPeakGrid(int[][] arr) {
-        int n= arr.length;
-        int m=arr[0].length;
-        
-        int low =0;
-        int high =n-1;
+    public int[] findPeakGrid(int[][] mat) {
 
+        int n = mat.length;
+        int m = mat[0].length;
+        int low = 0;
+        int high = n - 1;
 
-        while(low<=high){
-            int mid =low+(high-low)/2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int col = max(mat, mid);
 
-            //max el in mid row
-            int col =maxElement(arr, mid);
-            int up = mid - 1 >= 0 ? arr[mid - 1][col] : Integer.MIN_VALUE;
+            int up = mid - 1 >= 0 ? mat[mid - 1][col] : Integer.MIN_VALUE;
+            int down = mid + 1 < n ? mat[mid + 1][col] : Integer.MIN_VALUE;
 
-            int down = mid + 1 < n ? arr[mid + 1][col] : Integer.MIN_VALUE;
-
-
-            if (arr[mid][col] > up &&
-                arr[mid][col] > down) {
-
-                return new int[]{mid, col};
-            }
-            //move up
-             else if (up > arr[mid][col]) {
+            if (mat[mid][col] > up && mat[mid][col] > down) {
+                return new int[] { mid, col };
+            } else if (up > mat[mid][col]) {
 
                 high = mid - 1;
-            }
-
-            // down
-            else {
-
+            } else {
                 low = mid + 1;
             }
 
         }
-        return new int [] {-1,-1};
+        return new int[] { -1, -1 };
 
     }
 }
