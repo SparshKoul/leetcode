cf3ddeefed7924a08bd56a4259afa7d5f2dc4e29
Sparsh Koul <sparshkoul6@gmail.com>
2026-08-31 20:40:35 +0530

    Time: 8 ms (64.89%), Space: 45.6 MB (48.93%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,40 +1,37 @@
-import java.util.*;
-
 class Solution {
     public int[] frequencySort(int[] nums) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        HashMap<Integer,Integer> map =new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
+        
+        int ans [] = new int [nums.length];
+        int k=0;
 
-        int[] ans = new int[nums.length];
-        int k = 0;
-
-        while (!map.isEmpty()) {
+        while(!map.isEmpty()){
 
             int minFreq = Integer.MAX_VALUE;
             int maxNum = Integer.MIN_VALUE;
 
-            for (int num : map.keySet()) {
-                int freq = map.get(num);
+            for(int num : map.keySet()){
 
-                if (freq < minFreq) {
-                    minFreq = freq;
-                    maxNum = num;
-                } else if (freq == minFreq && num > maxNum) {
-                    maxNum = num;
+                int freq =map.get(num);
+                if(freq<minFreq){
+                    minFreq =freq;
+                    maxNum=num;
+                }
+                else if (freq==minFreq && num >maxNum){
+                    maxNum=num;
                 }
             }
 
-            for (int i = 0; i < minFreq; i++) {
-                ans[k++] = maxNum;
+            for(int i=0;i<minFreq;i++){
+                ans[k] =maxNum;
+                k++;
             }
-
             map.remove(maxNum);
         }
-
         return ans;
     }
 }
