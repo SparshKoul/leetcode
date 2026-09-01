38a42eedf8a2ffeb281b7d346b5c1f122c7618a6
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-02 02:52:31 +0530

    Time: 2074 ms (62.93%), Space: 42.3 MB (25.88%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        int n=num;
+        for(int i=1;i<num;i++){
+            if(num%i ==0){
+                sum+=i;
+            }
+        }
+        if(sum ==n)return true;
+        return false;
+        
+    }
+}
