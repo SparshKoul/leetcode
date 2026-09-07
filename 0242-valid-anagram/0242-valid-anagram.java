e85bbaf940e6ce0738e2a974acb5e13d1b116dec
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-07 19:13:50 +0530

    Time: 18 ms (13.59%), Space: 46.7 MB (9.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,33 +1,26 @@
-import java.util.*;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-
         if (s.length() != t.length()) {
             return false;
         }
-
         HashMap<Character, Integer> map = new HashMap<>();
 
-        // Count in s
-        for (char ch : s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        // Remove using t
-        for (char ch : t.toCharArray()) {
-
-            if (!map.containsKey(ch)) {
+        for(int i=0;i<t.length();i++){
+            char x =t.charAt(i);
+            if(!map.containsKey(x)){
                 return false;
             }
+            map.put(x,map.get(x)-1);
 
-            map.put(ch, map.get(ch) - 1);
-
-            if (map.get(ch) == 0) {
-                map.remove(ch);
+            if(map.get(x) ==0){
+                map.remove(x);
             }
         }
-
-        return map.isEmpty();
+        return true;
     }
 }
