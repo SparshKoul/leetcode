345db0162267d861654f8a6d3433eae79992dfc7
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-07 20:59:11 +0530

    Time: 13 ms (74.25%), Space: 46.5 MB (63.73%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,38 +1,32 @@
 class Solution {
     public String frequencySort(String s) {
         HashMap<Character,Integer> map = new HashMap<>();
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            char ch = s.charAt(i);
+        for(int i=0;i<s.length();i++){
+            char ch =s.charAt(i);
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
-
+        
         StringBuilder sb =new StringBuilder();
-
-        int k=0;
-        while(!map.isEmpty()){
-            int maxFreq = Integer.MIN_VALUE;
-            char maxchar = ' ';
-
+        while( !map.isEmpty()){
+            int maxfreq =Integer.MIN_VALUE;
+            char maxchar= ' ';
             for(char num : map.keySet()){
-
-                int freq =map.get(num);
-                if(freq> maxFreq){
-                    maxFreq=freq;
+                int freq=map.get(num);
+                if(freq>maxfreq){
+                    maxfreq=freq;
                     maxchar=num;
                 }
-                else if(freq==maxFreq && num >maxchar){
+                else if (freq ==maxfreq && num >maxchar){
                     maxchar=num;
                 }
-
+                
             }
-
-            for(int i=0;i<maxFreq;i++){
+            for(int i=0;i<maxfreq;i++){
                 sb.append(maxchar);
             }
             map.remove(maxchar);
         }
+
         return sb.toString();
-        
     }
 }
