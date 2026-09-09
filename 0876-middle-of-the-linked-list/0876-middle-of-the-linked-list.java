6e228decb0ce1d452af09a47e67abd9785b85b71
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-09 20:15:02 +0530

    Time: 0 ms (100%), Space: 42.9 MB (34.33%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,16 +10,15 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head==null || head.next==null){
-            return head;
-        }
         ListNode slow=head;
-        ListNode fast=head;
-
-        while(fast !=null && fast.next!=null){
+        //ListNode fast =head.next;// lc wants end half first
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
+
         }
         return slow;
+        
     }
 }
