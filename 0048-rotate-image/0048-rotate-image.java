5959d8dfce0f218ed0ab07ebeb89d7c6234dab79
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-10 20:30:28 +0530

    Time: 0 ms (100%), Space: 43.6 MB (70.75%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -11,7 +11,7 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-        //reve row
+        //rev row
         for (int i = 0; i < n; i++) {
             int left = 0;
             int right = n - 1;
