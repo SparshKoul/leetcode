c4594b993eba4ac8cabe08bd6924ccd02602d179
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-16 02:15:36 +0530

    Time: 4 ms (60.28%), Space: 50.1 MB (72.42%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,39 +1,29 @@
 class Solution {
-    public int path(int[][] grid, int i, int j, int[][] dp) {
-
-        //base eventuallyy we reach 0,0
-        if (i == 0 && j == 0) {
-            return grid[0][0];
-        }
-        //if we at top we dont to at row<0
-        if (i == 0) {
-            return grid[i][j] + path(grid, i, j - 1, dp);
-        }
-        if (j == 0) {
-            return grid[i][j] + path(grid, i - 1, j, dp);
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        dp[i][j] = grid[i][j] + Math.min(
-                path(grid, i - 1, j, dp),
-                path(grid, i, j - 1, dp));
-
-        return dp[i][j];
-
-    }
-
     public int minPathSum(int[][] grid) {
 
-        int m = grid.length;
-        int n = grid[0].length;
-        int dp[][] = new int[m][n];
-        
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+        int m=grid.length;
+        int n =grid[0].length;
+
+        int [][] dp = new int [m][n];
+        dp[0][0] =grid[0][0];
+
+
+        //row
+        for(int i=1;i<n;i++){
+            dp[0][i] = grid[0][i] + dp[0][i-1];
         }
 
-        return path(grid, m - 1, n - 1, dp);
+        //col
+        for(int i=1;i<m;i++){
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
 
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = grid[i][j] +Math.min(dp[i-1][j], dp[i][j-1]);
+           }
+        }
+        return dp[m-1][n-1];
+        
     }
 }
