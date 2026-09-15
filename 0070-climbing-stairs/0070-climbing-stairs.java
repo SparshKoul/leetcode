914c46c16d24845dc62ab1758182254a6f369256
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-15 18:51:16 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int ways(int n,int []dp){
-        
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if(dp[n] !=-1){
-            return dp[n];
-        }
-        dp[n]= ways(n-1,dp) + ways(n-2,dp);
-        return dp[n];
-    }
     public int climbStairs(int n) {
-        int dp[] = new int [n+1];
-        Arrays.fill(dp,-1);
-        return ways(n,dp);
+        if (n == 1) {
+            return 1;
+        }
 
-        
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 }
