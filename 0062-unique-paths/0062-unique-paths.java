c7c48752d1774f9f692af869ee22581d28b3f499
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-21 20:11:20 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,31 +1,30 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       //sapce opt
+        //sapce opt2 
         int dp[][] = new int[2][n];
 
-        
         for (int j = 0; j < n; j++) {
             dp[0][j] = 1;
-            dp[1][j] =1;
+            dp[1][j] = 1;
         }
-
-       
 
         for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[1][j] = dp[0][j] + dp[1][j - 1];
 
+            if (i % 2 == 0) {
+
+                for (int j = 1; j < n; j++) {
+                    dp[1][j] = dp[0][j] + dp[1][j - 1];
+
+                }
+            } else {
+                for (int j = 1; j < n; j++) {
+                    dp[0][j] = dp[0][j - 1] + dp[1][j];
+
+                }
             }
-
-            //coplying/sliding
-            for (int j = 1; j < n; j++) {
-                dp[0][j] = dp[1][j] ;
-
-            }
-
 
         }
-        return dp[1][n - 1];
+        return Math.max(dp[1][n - 1], dp[0][n - 1]);
 
     }
 }
