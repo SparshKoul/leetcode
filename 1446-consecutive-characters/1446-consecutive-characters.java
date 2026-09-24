a8f12dc6ba4266862ece31a0dd5ae6d3dc2a693b
Sparsh Koul <sparshkoul6@gmail.com>
2026-09-24 20:44:50 +0530

    Time: 1 ms (100%), Space: 42.7 MB (99.44%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxPower(String s) {
+        int max =1;
+        int count =1;
+        for(int i=0;i<s.length()-1;i++){
+            
+            if(s.charAt(i)==s.charAt(i+1)){
+                count++;
+                max=Math.max(count,max);
+            }else{
+                count =1;
+            }
+
+        }return max;
+        
+    }
+}
